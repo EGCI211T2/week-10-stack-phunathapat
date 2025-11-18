f25cdc55d47ea75c6df0bfcc2a126e62bebc8b94
phunathapat <126891999+phunathapat@users.noreply.github.com>
2025-11-18 03:44:06 +0000

    Final

--- main.cpp
+++ main.cpp
@@ -12,15 +12,15 @@ int main(int argc, char **argv){
       s.push(6);
       s.pop();
 
- /*
-  Exercise 1*/
-    /*
+ 
+  //Exercise 1
+    
     int i;
     for(i=1;i<argc;i++){
-      //  if(  x)  s.pop();
+        if( argv [i][0]== 'x')  s.pop(); //
         else s.push(atoi(argv[i]));
     }
-     */
+     
 
 
 
